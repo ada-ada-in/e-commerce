c6e777a7946e26b22b6341863c5b19561a90e7f1
Chandra Halim <chan.halim98@gmail.com>
2025-03-12 21:44:30 +0700

    add dashboard

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,12 +9,6 @@ use CodeIgniter\Router\RouteCollection;
 // admin
 $routes->group('admin', static function($routes) {
     $routes->get('dashboard', 'PagesController::dashboard', ['as' => 'dashboard']);
-    $routes->get('surat-masuk', 'PagesController::suratMasuk', ['as' => 'suratMasuk']);
-    $routes->get('surat-keluar', 'PagesController::suratKeluar', ['as' => 'suratKeluar']);
-    $routes->get('disposisi', 'PagesController::disposisi', ['as' => 'disposisi']);
-    $routes->get('pengguna', 'PagesController::pengguna', ['as' => 'pengguna']);
-    $routes->get('arsip', 'PagesController::arsip', ['as' => 'arsip']);
-    $routes->get('profile', 'PagesController::profile', ['as' => 'profile']);
 });
 
 
